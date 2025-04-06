fc93875424e837487da1544d1e0ccaf2fd2672ab
Jiarong Tang <57526773+jiarongTang@users.noreply.github.com>
2025-04-06 18:33:25 +0200

    cambios1

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -6,15 +6,11 @@ export const Home = () => {
   cleanPage(main);
   main.innerHTML = `
     <section class="home">
-    <p>Hey, I'm</p>
-    <h1>Peter Parker</h1>
-    <p>I'm a software engineer based in Toronto, 
-    Canada and also a communication and journalism student. 
-    I enjoy creating things that live on the internet, 
-    whether that be websites, applications, or anything in between. 
-    I have been freelancing for a year now while studying at the university and 
-    I've manage to gain a decent amount of experience and valuable knowledge 
-    from all different kinds of fields throughout my projects/work.</p>
-    <a href="mailto:peterparker@gmail.com">Say hi →</a>
+    <p>Hola, soy </p>
+    <h1>Jiarong Tang</h1>
+    <p class="texto">
+      Soy una estudiante de grado superior de Desarrollo de Aplicaciones Web en Madrid, España. He estado tambien estudiando en la universida en el grado de Ingieneria Informatica, pero no esta finalizado por problemas personales. Los hobbies que tengo son las manualidades de cualquiertipo (origami, crochett,...). He trabajado como dependiente en tienda de venta de piezas de moviles y en bazares, y cajera en almacenes. Tengo manejo en diferentes lenguajes de programacion como html, css y javaScript. Con respecto a los lenguajes que controlo seria el chino y el españo con nivel nativo; y el ingles con nivel de B2. En este portfolio hay alguno de los proyectos que he realizado.
+    </p>
+    <a href="mailto:jiarong.tang.daw@gmail.com">Correo →</a>
     </section>`;
 };
