493d7c9ca9798ce884d85481d16a511f6d949a8c
Jiarong Tang <57526773+jiarongTang@users.noreply.github.com>
2025-04-27 18:27:00 +0200

    cambios

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
     <p>Hola, soy </p>
     <h1>Jiarong Tang</h1>
     <p class="texto">
-      Soy una estudiante de grado superior de Desarrollo de Aplicaciones Web en Madrid, España. He estado tambien estudiando en la universida en el grado de Ingieneria Informatica, pero no esta finalizado por problemas personales. Los hobbies que tengo son las manualidades de cualquiertipo (origami, crochett,...). He trabajado como dependiente en tienda de venta de piezas de moviles y en bazares, y cajera en almacenes. Tengo manejo en diferentes lenguajes de programacion como html, css y javaScript. Con respecto a los lenguajes que controlo seria el chino y el españo con nivel nativo; y el ingles con nivel de B2. En este portfolio hay alguno de los proyectos que he realizado.
+      Soy una estudiante de grado superior de Desarrollo de Aplicaciones Web en Madrid, España. He estado también estudiando en la universida en el grado de Ingienería Informática, pero no está finalizado por problemas personales. Los hobbies que tengo son las manualidades de cualquier tipo (origami, crochett,...). He trabajado como dependiente en tienda de venta de piezas de móviles, en bazares, y como cajera en almacenes. Tengo manejo en diferentes lenguajes de programación como html, css y javaScript. Con respecto a los lenguajes que controlo seria el chino y el españo con nivel nativo; y el inglés con nivel B2. En este portfolio hay alguno de los proyectos que he realizado.
     </p>
     <a href="mailto:jiarong.tang.daw@gmail.com">Enviar un correo →</a>
     </section>`;
