9fe903daa978960fc77e20e4a2b8fff5c77716fe
Jiarong Tang <57526773+jiarongTang@users.noreply.github.com>
2025-04-27 19:04:26 +0200

    minCambioIMG

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
       title: "Pokemon Iniciales",
       description: "Mini enciclopedia de pokemon inicales de cada region con sus evoluciones hasta la temporada más actual",
       image:
-        "../public/proyecto1.PNG",
+        "/proyecto1.PNG",
       tech: [ "HTML5", "CSS3"],
       github: "https://github.com/jiarongTang/proyecto1web.git",
       link: "https://pokemoniniciales.netlify.app/",
@@ -12,7 +12,7 @@ export const projects = [
       title: "Pokevilla",
       description: "Proyecto de creacion de una tiendo con disposicion de diversos producto con funcion de filtros tanto de busqueda como por seleccion segun el tipo de articulo",
       image:
-        "../public/proyecto2.PNG",
+        "/proyecto2.PNG",
       tech: ["HTML5", "CSS3", "JavaScript"],
       github: "https://github.com/jiarongTang/practicaDOM.git",
       link: "https://pokevilla.netlify.app/",
