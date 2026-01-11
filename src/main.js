bd301b6241ba02b912157bb79b370d679f984a04
Jiarong Tang <187628173+JiarongTangDaw@users.noreply.github.com>
2026-01-11 14:42:31 +0100

    cambio2

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { Navbar } from "../components/Navbar/Navbar";
 import { Footer } from "../components/Footer/Footer";
 import { Home } from "../pages/Home/Home";
 import { Projects } from "../pages/Projects/Projects";
-import { About_Me } from '../pages/About_Me/about_me';
+import { About_Me } from "../pages/About_Me/About_Me";
 import { Divider } from "../components/Divider/Divider";
 
 const header = document.querySelector("header");
