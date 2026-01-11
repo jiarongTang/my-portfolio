29caa307d76594241c79780240b4e16806afeca8
Jiarong Tang <>
2026-01-11 15:35:37 +0100

    cambio5

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,6 +6,6 @@ export const Footer = () => `
 <h2>Contact</h2>
 <div>
 ${Button("/github.jpeg", "GitHub","https://github.com/jiarongTang")}
-${Button("/linkelin.jpeg", "LinkedIn","https://www.linkedin.com/feed/")}
+${Button("/linkelin.jpeg", "LinkedIn","https://www.linkedin.com/in/jiarong-tang-859757160/")}
 </div>
 `;
