e6e96c319701bb3bddbaebdf2160bb54522e4d2d
Jiarong <jiarongrosa@gmail.com>
2026-01-15 08:55:23 +0100

    curriculum2

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -12,6 +12,6 @@ export const Home = () => {
       Estudiante de Desarrollo de Aplicaciones Web y Máster de Full Stack, con conocimientos en base de datos y experiencia en lenguajes como Java, PHP y JavaScript. Apasionada por el desarrollo de interfaces web empleando lenguaje de marcas. En busca de trabajo y ganar experiencia en el sector tecnológico.
     </p>
     <a href="mailto:jiarong.tang.daw@gmail.com">Enviar un correo →</a>
-    <a href="../../public/curriculum.pdf" download="curriculum_jiarong_tang.pdf" class="boton-curriculum">Descargar CV</a>
+    <a href="/curriculum.pdf" download="curriculum_jiarong_tang.pdf" class="boton-curriculum">Descargar CV</a>
     </section>`;
 };
